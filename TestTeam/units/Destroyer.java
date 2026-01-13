eefd255043888bd6c66ddf1a9609dbbbec4bd646
selosteamingGit <seanlookingbill26@hotmail.com>
2026-01-13 18:08:12 -0500

    Bug Fixes + not working commander for some reason

---
+++ TestTeam/units/Destroyer.java
@@ -0,0 +1,25 @@
+package teams.student.TestTeam.units;
+
+import components.mod.offense.CerberusMod;
+import components.upgrade.Shield;
+import components.weapon.energy.Laser;
+import components.weapon.explosive.Missile;
+import components.weapon.utility.SpeedBoost;
+import objects.entity.unit.Frame;
+import objects.entity.unit.Model;
+import objects.entity.unit.Style;
+import teams.student.TestTeam.TestTeamUnit;
+
+public class Destroyer extends TestTeamUnit {
+
+    public void design() {
+        setFrame(Frame.MEDIUM);
+        setModel(Model.ARTILLERY);
+        setStyle(Style.ARROW);
+
+        add(Laser.class);
+        add(SpeedBoost.class);
+        add(Shield.class);
+    }
+
+}
---
+++ TestTeam/units/resource/ResourceGrabber.java
@@ -0,0 +1,45 @@
+package teams.student.TestTeam.units.resource;
+
+import components.weapon.economy.Collector;
+import objects.entity.unit.Frame;
+import objects.entity.unit.Model;
+import objects.entity.unit.Style;
+import objects.resource.Resource;
+import org.newdawn.slick.Graphics;
+import teams.student.TestTeam.TestTeamUnit;
+
+public class ResourceGrabber extends TestTeamUnit {
+
+    public void design()
+    {
+        setFrame(Frame.MEDIUM);
+        setModel(Model.PROTOTYPE);
+        setStyle(Style.ORB);
+        add(Collector.class);
+        add(Collector.class);
+    }
+
+    public void action()
+    {
+        moveTo(getHomeBase().getCenterX(), getHomeBase().getCenterY());
+
+        Resource r = getNearestResource();
+        Resource r2 = getNearestResource();
+
+        ((Collector) getWeaponOne()).use(r);
+
+        for (Resource res : getResourcesInRadius(getHomeBase().getWidth(), this)){
+            if (res != r){
+                r2 = res;
+            }
+        }
+        ((Collector) getWeaponTwo()).use(r2);
+
+        deposit();
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+}
